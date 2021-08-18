184703ac1642856ade33a8f45adb8f8d05729897
Feldwor <BoQsc@users.noreply.github.com>
2021-08-18 08:01:27 +0000

    Create add.bash

---
+++ add.bash
@@ -0,0 +1,26 @@
+# Before we introduce our XFCE Recipe to the local Recipe repository that is /Data/Compile/Recipe
+# We need to fetch the Recipes from https://github.com/gobolinux/Recipes 
+Compile
+
+# Downloading the Recipe
+curl -L "https://github.com/vaido-world/GoboLinux-Recipe-XFCE/raw/main/XFCE-Meta-Stable.tar.gz" -O
+
+# Extracting the Recipe from the Archive
+# unzip "XFCE-Meta-Stable.zip"
+tar --extract --file="XFCE-Meta-Stable.tar.gz" --gzip --verbose
+
+# Move our XFCE Recipe to the Local Recipes Repository
+mv "XFCE-Meta-Stable/" "/Data/Compile/Recipes/"
+
+# Install UnionFS
+InstallPackage https://gobolinux.org/packages/017/Fuse--2.9.7--x86_64.tar.bz2
+InstallPackage https://gobolinux.org/packages/017/UnionFS-Fuse--2.1--x86_64.tar.bz2
+
+# Fix docbook for GoboLinux 17 release
+sed -i '/delegatePublic/c\' /Data/Variable/lib/xml/catalog  
+sed -i '/delegateSystem/c\' /Data/Variable/lib/xml/catalog  
+sed -i '/delegateURI/c\' /Data/Variable/lib/xml/catalog
+Compile Docbook-xml-dtd --no-dependencies --batch
+
+# Compile the XFCE Desktop Environment using XFCE Meta Recipe
+Compile "XFCE-Meta-Stable"
