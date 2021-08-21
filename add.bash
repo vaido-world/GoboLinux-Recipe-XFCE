b69778c6a6b931f3afff50939513a12f95622f0f
Feldwor <BoQsc@users.noreply.github.com>
2021-08-21 17:59:45 +0000

    Update add.bash

--- add.bash
+++ add.bash
@@ -116,6 +116,9 @@ InstallPackage https://gobolinux.org/packages/017/UnionFS-Fuse--2.1--x86_64.tar.
 # Required before XFCE4-Panel 4.14.0 Compilation
 Compile LibWNCK 3.36.0 
 
+
+# Required for LibGUdev 234 version
+Compile GTK-Doc 1.33.0
 # Thunar-Volman Failed and requested for gudev-1.0 >= 145 when with --no-dependencies
 Compile LibGUdev 234
 
