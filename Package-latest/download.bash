5c719cdc8b1230c88b657d57dd27d1efb37d129a
Feldwor <BoQsc@users.noreply.github.com>
2021-08-23 19:38:47 +0300

    Create download.bash

---
+++ Package-latest/download.bash
@@ -0,0 +1,8 @@
+curl -O https://github.com/vaido-world/GoboLinux-Recipe-XFCE/raw/main/Package-latest/Dependencies/DocBook-XML-DTD--4.5--x86_64.tar.bz2
+curl -O https://github.com/vaido-world/GoboLinux-Recipe-XFCE/raw/main/Package-latest/Dependencies/GTK-Doc--1.33.0--x86_64.tar.bz2
+curl -O https://github.com/vaido-world/GoboLinux-Recipe-XFCE/raw/main/Package-latest/Dependencies/LibGUdev--234--x86_64.tar.bz2
+curl -O https://github.com/vaido-world/GoboLinux-Recipe-XFCE/raw/main/Package-latest/Dependencies/LibWNCK--3.36.0--x86_64.tar.bz2
+curl -O https://github.com/vaido-world/GoboLinux-Recipe-XFCE/raw/main/Package-latest/Dependencies/UPower--0.99.11--x86_64.tar.bz2
+curl -O https://github.com/vaido-world/GoboLinux-Recipe-XFCE/raw/main/Package-latest/XFCE-Meta-Stable--4.14--x86_64.tar.bz2
+
+
