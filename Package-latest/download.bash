1532346dab88d6749c37557367695c861abba0fd
Feldwor <BoQsc@users.noreply.github.com>
2021-08-23 16:56:40 +0000

    Update download.bash

--- Package-latest/download.bash
+++ Package-latest/download.bash
@@ -5,4 +5,7 @@ curl --location -O https://github.com/vaido-world/GoboLinux-Recipe-XFCE/raw/main
 curl --location -O https://github.com/vaido-world/GoboLinux-Recipe-XFCE/raw/main/Package-latest/Dependencies/UPower--0.99.11--x86_64.tar.bz2
 curl --location -O https://github.com/vaido-world/GoboLinux-Recipe-XFCE/raw/main/Package-latest/XFCE-Meta-Stable--4.14--x86_64.tar.bz2
 
-
+InstallPackage XFCE-Meta-Stable--4.14--x86_64.tar.bz2
+InstallPackage LibWNCK--3.36.0--x86_64.tar.bz2
+InstallPackage LibGUdev--234--x86_64.tar.bz2
+InstallPackage UPower--0.99.11--x86_64.tar.bz2
