883d5a16962b25b9b2480ce236d95c1e60588a45
Feldwor <BoQsc@users.noreply.github.com>
2021-08-23 17:02:06 +0000

    Update download.bash

--- Package-latest/download.bash
+++ Package-latest/download.bash
@@ -1,5 +1,5 @@
-curl --location -O https://github.com/vaido-world/GoboLinux-Recipe-XFCE/raw/main/Package-latest/Dependencies/DocBook-XML-DTD--4.5--x86_64.tar.bz2
-curl --location -O https://github.com/vaido-world/GoboLinux-Recipe-XFCE/raw/main/Package-latest/Dependencies/GTK-Doc--1.33.0--x86_64.tar.bz2
+# curl --location -O https://github.com/vaido-world/GoboLinux-Recipe-XFCE/raw/main/Package-latest/Dependencies/DocBook-XML-DTD--4.5--x86_64.tar.bz2
+# curl --location -O https://github.com/vaido-world/GoboLinux-Recipe-XFCE/raw/main/Package-latest/Dependencies/GTK-Doc--1.33.0--x86_64.tar.bz2
 curl --location -O https://github.com/vaido-world/GoboLinux-Recipe-XFCE/raw/main/Package-latest/Dependencies/LibGUdev--234--x86_64.tar.bz2
 curl --location -O https://github.com/vaido-world/GoboLinux-Recipe-XFCE/raw/main/Package-latest/Dependencies/LibWNCK--3.36.0--x86_64.tar.bz2
 curl --location -O https://github.com/vaido-world/GoboLinux-Recipe-XFCE/raw/main/Package-latest/Dependencies/UPower--0.99.11--x86_64.tar.bz2
